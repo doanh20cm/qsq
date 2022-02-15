bb9736aa8fbbae02dcebd6fb6eb235aa95722f55
doanh20cm <97477998+doanh20cm@users.noreply.github.com>
2022-02-15 16:49:56 +0700

    Update Qsq.java

--- src/qsq/Qsq.java
+++ src/qsq/Qsq.java
@@ -1,21 +1,4 @@
-<<<<<<< HEAD
-=======
+
 package 1233443;
 
 public class Qsq {
-    
-    
-    
->>>>>>> 0809f7b87d4eea69949c40f9a52ed451da3d636b
-    public static int qsq(int, int) {
-        return 0 + 1 +2 ;
-        
-        
-        
-        
-        
-        
-        
-        /// mới thêm chỗ này
-    }
-}
